3c9a43d21036df029c512113882ed6324686f786
deathdayss <45457242+deathdayss@users.noreply.github.com>
2021-07-09 22:34:24 +0800

    Add language data

---
+++ src/data/words/Chinese.js
@@ -0,0 +1,8 @@
+export const Chinese = {
+    "header": {
+        "all": "全部",
+        "fanMade": "二创",
+        "realWorld": "三次元",
+        "traditional": "传统"
+    } 
+}
---
+++ src/data/words/English.js
@@ -0,0 +1,8 @@
+export const English = {
+    "header": {
+        "all": "All",
+        "fanMade": "Fanmade",
+        "realWorld": "Real World",
+        "traditional": "Traditional"
+    } 
+}
