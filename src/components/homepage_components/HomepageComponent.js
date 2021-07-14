e12b2e5fd4c992047f9845214bd9ca13e158144d
deathdayss <45457242+deathdayss@users.noreply.github.com>
2021-07-14 17:21:54 +0800

    play with @rebase/grid library

--- src/components/homepage_components/HomepageComponent.js
+++ src/components/homepage_components/HomepageComponent.js
@@ -1,6 +1,9 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import { withRouter } from 'react-router-dom'
 import { Col, Container, Row } from 'reactstrap'
+import { Flex, Box } from '@rebass/grid'
+
+import { ThemeProvider } from 'styled-components'
 
 import './HomepageComponent.scss'
 
@@ -11,26 +14,84 @@ class Homepage extends Component {
 
     }
     render() {
+        const theme = {
+            breakpoints: [
+                '576px',
+                '768px',
+                '992px',
+                '1200px'
+            ],
+            space: '12'
+        }
         return (
-            <Container fluid  id='main-page-container'>
-                <Row className=''>
-                    <Col>
-                    1
-                    </Col>
-                    <Col>
-                    2
-                    </Col>
-                    <Col>
-                    3
-                    </Col>
-                    <Col>
-                    4
-                    </Col>
-                    <Col>
-                    5
-                    </Col>
-                </Row>
-            </Container>
+            <>
+            <Flex>
+                <Box width={0.5} mx = {'30px'}>
+                    Box 1
+                </Box>
+                <Box width={0.5} mx = {'30px'}>
+                    Box 2
+                </Box>
+            </Flex>
+                {/* <Flex mt={10} className='text-center'>
+                    <Box width={0.7}>
+                    </Box>
+                    <Box width={0.2}>
+                        <Flex>
+                            <Box width={1 / 3} px={3}>
+                                <button id='' className='rank-time-btn'>
+                                    1234
+                                </button>
+                            </Box>
+                            <Box width={1 / 3} px={3}>
+                                <button id='' className='rank-time-btn'>
+                                    1234
+                                </button>
+                            </Box>
+                            <Box width={1 / 3} px={3}>
+                                <button id='' className='rank-time-btn'>
+                                    1234
+                                </button>
+                            </Box>
+
+                        </Flex>
+                    </Box>
+                </Flex>
+                <Flex mx={20} className = 'text-center'>
+                    <Box width={0.4} mx={'16px'}>
+                        <Flex py ={'23px'}>
+                            <Box width={1}>
+                                <img src='fake_data/work_cover.jpg' width='664' height='360' />
+                            </Box>
+                        </Flex>
+                        <Flex py = {'23px'} className='text-start'>
+                            <Box width={0.5} mx = {'10px'}>
+                                <img src='fake_data/work_cover.jpg' width='320' height='180' />
+                            </Box>
+                            <Box width={0.5} mx = {'10px'}>
+                                <img src='fake_data/work_cover.jpg' width='320' height='180' />
+                            </Box>
+                        </Flex>
+                    </Box>
+                    <Box width={0.4} px={'16px'}>
+                        123
+                    </Box>
+                    <Box width={0.2} px={'16px'}>
+                        123
+                    </Box>
+                </Flex> */}
+
+                {/* <ThemeProvider theme={theme}>
+                    <Flex>
+                        <Box width={0.5}>
+                            123
+                        </Box>
+                        <Box width={0.5} px='auto'>
+                            123
+                        </Box>
+                    </Flex>
+                </ThemeProvider> */}
+            </>
         )
     }
 }
