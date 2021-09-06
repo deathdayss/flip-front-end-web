6d0c7000c1055c90703c10ba8877dbbe92fbf1c7
Robert Zhao <u7149863@anu.edu.au>
2021-09-06 09:38:55 +0800

    upd: drag upload style updated

--- src/components/upload_components/DragUpload.jsx
+++ src/components/upload_components/DragUpload.jsx
@@ -1,58 +1,75 @@
 /**
- * @author Suowei Hu
- * @email suowei.h@gmail.com
+ * @author Suowei Hu, Robert Zhao
+ * @email suowei.h@gmail.com, vibrantrobert@gmail.com
  * @create date 2021-08-11 18:18:24
- * @modify date 2021-08-11 18:18:24
+ * @modify date 2021-08-27 20:00:00
  * @desc [description]
  */
 
-import React from 'react'
+ import React,{Component} from 'react'
+ 
+ import Header from '../header_components/Header.jsx';
+ import { Upload, message } from 'antd';
+ import { UploadOutlined } from '@ant-design/icons';
+ 
+ const { Dragger } = Upload;
+ 
+ 
+ 
+ // TODO: Read the documents and finish the remaiing upload "HTTP-Posting" stuff  
+ // https://ant.design/components/upload-cn/
+ 
+ const props = {
+   className: 'uploader',
+   name: 'file',
+   multiple: false,
+   action: 'https://www.mocky.io/v2/5cc8019d300000980a055e76',
+   onChange(info) {
+     const { status } = info.file;
+     if (status !== 'uploading') {
+       console.log(info.file, info.fileList);
+     }
+     if (status === 'done') {
+       message.success(`${info.file.name} file uploaded successfully.`);
+     } else if (status === 'error') {
+       message.error(`${info.file.name} file upload failed.`);
+     }
+   },
+   onDrop(e) {
+     console.log('Dropped files', e.dataTransfer.files);
+   },
+ };
 
-import { Upload, message } from 'antd';
-import { InboxOutlined } from '@ant-design/icons';
-import './DragUpload.scss'
-
-const { Dragger } = Upload;
-
-// TODO: Read the documents and finish the remaiing upload "HTTP-Posting" stuff  
-// https://ant.design/components/upload-cn/
-
-const props = {
-  name: 'file',
-  multiple: true,
-  action: 'https://www.mocky.io/v2/5cc8019d300000980a055e76',
-  onChange(info) {
-    const { status } = info.file;
-    if (status !== 'uploading') {
-      console.log(info.file, info.fileList);
-    }
-    if (status === 'done') {
-      message.success(`${info.file.name} file uploaded successfully.`);
-    } else if (status === 'error') {
-      message.error(`${info.file.name} file upload failed.`);
-    }
-  },
-  onDrop(e) {
-    console.log('Dropped files', e.dataTransfer.files);
-  },
-};
-
-
-function DragUpload() {
-  return (
-    <div className="uploader-container">
-      <Dragger {...props}>
-        <p className="ant-upload-drag-icon">
-          <InboxOutlined />
-        </p>
-        <p className="ant-upload-text">Click or drag file to this area to upload</p>
-        <p className="ant-upload-hint">
-          Support for a single or bulk upload. Strictly prohibit from uploading company data or other
-          band files
-        </p>
-      </Dragger>
-    </div>
-  )
-}
-
-export default DragUpload
+ const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  margin: '-336px 0 0 -519px',height: '672px',width: '1038px'
+ }
+ 
+ 
+ class DragUpload extends Component {
+   render() {
+     return (
+       <div>
+         <Header />
+         <div style={style}>
+         <Dragger {...props} directory>
+           <p className="ant-upload-drag-icon">
+             <UploadOutlined />
+           </p>
+           <p className="ant-upload-text">Click or drag WebGL folder to this area to upload</p>
+           <p className="ant-upload-hint">
+             Support for a single or bulk upload. Strictly prohibit from uploading company data or other
+             band files
+           </p>
+         </Dragger>
+         </div>
+       </div>
+     )
+   }
+   
+ }
+ 
+ export default DragUpload
+ 
---
+++ src/components/upload_components/UploadForm1.jsx
@@ -0,0 +1,283 @@
+/**
+ * @author [Robert Zhao]
+ * @email 
+ * @create date 2021-08-27 23:32:13
+ * @modify date 2021-08-27 23:32:13
+ * @desc [description]
+ */
+
+ import React, { Component } from 'react';
+
+ import Header from '../header_components/Header.jsx';
+ 
+ import { Layout, Input, Form, Select, Modal, Button, Upload } from 'antd';
+ 
+ import { UploadOutlined, PlusOutlined } from '@ant-design/icons';
+ const { Option } = Select;
+ 
+ const formItemLayout = {
+     labelCol: {
+         span: 6,
+     },
+     wrapperCol: {
+         span: 14,
+     },
+ };
+ 
+ const normFile = (e) => {
+     console.log('Upload event:', e);
+ 
+     if (Array.isArray(e)) {
+         return e;
+     }
+ 
+     return e && e.fileList;
+ };
+ 
+ const handleUploadRequest = (values) => {
+    console.log("Form submitted");
+}
+
+//Miniature display
+function getBase64(file) {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = () => resolve(reader.result);
+      reader.onerror = error => reject(error);
+    });
+  }
+  
+  class PicturesWall extends Component {
+    state = {
+      previewVisible: false,
+      previewImage: '',
+      previewTitle: '',
+      fileList: [
+        // {
+        //   uid: '-1',
+        //   name: 'image.png',
+        //   status: 'done',
+        //   url: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
+        // },
+        // {
+        //   uid: '-2',
+        //   name: 'image.png',
+        //   status: 'done',
+        //   url: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
+        // },
+        // {
+        //   uid: '-3',
+        //   name: 'image.png',
+        //   status: 'done',
+        //   url: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
+        // },
+        // {
+        //   uid: '-4',
+        //   name: 'image.png',
+        //   status: 'done',
+        //   url: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
+        // },
+        // {
+        //   uid: '-xxx',
+        //   percent: 50,
+        //   name: 'image.png',
+        //   status: 'uploading',
+        //   url: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
+        // },
+        // {
+        //   uid: '-5',
+        //   name: 'image.png',
+        //   status: 'error',
+        // },
+      ],
+    };
+  
+    handleCancel = () => this.setState({ previewVisible: false });
+  
+    handlePreview = async file => {
+      if (!file.url && !file.preview) {
+        file.preview = await getBase64(file.originFileObj);
+      }
+  
+      this.setState({
+        previewImage: file.url || file.preview,
+        previewVisible: true,
+        previewTitle: file.name || file.url.substring(file.url.lastIndexOf('/') + 1),
+      });
+    };
+  
+    handleChange = ({ fileList }) => this.setState({ fileList });
+  
+    render() {
+      const { previewVisible, previewImage, fileList, previewTitle } = this.state;
+      const uploadButton = (
+        <div>
+          <PlusOutlined />
+          <div style={{ marginTop: 8 }}>Upload</div>
+        </div>
+      );
+      return (
+        <>
+          <Upload
+            action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+            listType="picture-card"
+            fileList={fileList}
+            onPreview={this.handlePreview}
+            onChange={this.handleChange}
+          >
+            {fileList.length > 1 ? null : uploadButton}
+          </Upload>
+          <Modal
+            visible={previewVisible}
+            title={previewTitle}
+            footer={null}
+            onCancel={this.handleCancel}
+          >
+            <img alt="example" style={{ width: '100%' }} src={previewImage} />
+          </Modal>
+        </>
+      );
+    }
+  }
+ 
+//  const handleUploadRequest = (history) => {
+//      const url = "http://192.168.1.13:5000/login";
+//      console.log("HTTP request made towards: " + url);
+//      // 第一个参数是提交的地址
+//      try {
+//          $.post(url,
+//              {
+//                  email: val_email,
+//                  password: val_passw
+//              },
+//              function (data, status) {
+//                  console.log(data);
+//                  var dataObj = eval("(" + data + ")");//转换为json对象 
+//                  if (dataObj.code === 200) {
+//                      console.log("LOGIN SUCCESS")
+//                      history.push('/');
+//                      return;
+//                  } else {
+//                      console.log("LOGIN FAILURE")
+//                      alert(dataObj.msg)
+//                      message.warn('Either your <email> or <password> is incorrect', 0.6);
+//                      return;
+//                  }
+//              }
+//          ).fail(function(){message.warn('Connection to server is a failure', 0.6); if(fail_forward){history.push('/')} }); 
+//      } catch (e) {
+//          message.warn('Connection to server is a failure', 0.6);
+//      }
+//  }
+ 
+ 
+ const { Footer, Sider, Content } = Layout;
+ 
+ class UploadForm1 extends Component {
+     render() {
+         return (
+             <Layout>
+                 <Header />
+                 <Content>
+                     <div>
+                         <Form
+                             name="validate_other"
+                             {...formItemLayout}
+                             onFinish={handleUploadRequest}
+                             initialValues={{
+                                 'input-number': 3,
+                                 'checkbox-group': ['A', 'B'],
+                                 rate: 3.5,
+                             }}
+                         >
+                             <Form.Item label="File Name">
+                                 <span className="ant-form-text"></span>
+                             </Form.Item>
+ 
+                             <Form.Item
+                                 name="cover_upload"
+                                 label="Cover"
+                                 valuePropName="fileList"
+                                 getValueFromEvent={normFile}
+                                 extra=""
+                             >
+                                 <PicturesWall />
+                                 
+                                 <PicturesWall />
+                             </Form.Item>
+ 
+                             <Form.Item
+                                 {...formItemLayout}
+                                 name="title"
+                                 label="Title"
+                                 rules={[
+                                     {
+                                         required: true,
+                                         message: 'Please input the title',
+                                     },
+                                 ]}
+                             >
+                                 <Input placeholder="100/100" />
+                             </Form.Item>
+ 
+                             <Form.Item
+                                 name="select"
+                                 label="Classification"
+                                 hasFeedback
+                                 rules={[
+                                     {
+                                         required: true,
+                                         message: 'Please select the section!',
+                                     },
+                                 ]}
+                                 wrapperCol={{
+                                         span: 2,
+                                 
+                                     }}
+                             >
+                                 <Select placeholder="Please select a section">
+                                     <Option value="renew">二创</Option>
+                                     <Option value="3d">三次元</Option>
+                                     <Option value="classic">传统</Option>
+                                 </Select>
+                             </Form.Item>
+ 
+ 
+                             <Form.Item
+                                 {...formItemLayout}
+                                 name="description"
+                                 label="Description"
+                                 rules={[
+                                     {
+                                         required: true,
+                                         message: 'Please input the title',
+                                     },
+                                 ]}
+                             >
+                             <Input value = " " placeholder="100/100" />
+                             </Form.Item>
+ 
+ 
+ 
+ 
+                                 <Form.Item
+                                     wrapperCol={{
+                                         span: 12,
+                                         offset: 6,
+                                     }}
+                                 >
+                                     <Button type="primary" htmlType="submit">
+                                         Submit
+                                     </Button>
+                                 </Form.Item>
+                         </Form>
+                     </div>
+                 </Content>
+                     <Footer>Footer</Footer>
+             </Layout>
+                 )
+     }
+ }
+ 
+                 export default UploadForm1;
