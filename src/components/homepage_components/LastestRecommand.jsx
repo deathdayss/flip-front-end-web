b560204a4deba3d8cfa240d0a03b4f8eb5bd260f
SuoweiHu <suowei.h@gmail.com>
2021-09-11 15:00:19 +0800

    feat: "Latest & Recommendation" section of the main page (request

--- src/components/homepage_components/LastestRecommand.jsx
+++ src/components/homepage_components/LastestRecommand.jsx
@@ -4,25 +4,38 @@
  * @modify date 2021-07-24 21:15:48
  */
 
-import React, { Component, useState } from 'react'
+import React, { Component, useState, useEffect } from 'react'
 import { Flex, Box } from '@rebass/grid'
 
 import { ForLoop } from '../helper_components/Helper.jsx'
 import { homepageSpacing } from '../../data/constants/Spacing'
 import './LastestRecommand.scss'
 import request from 'umi-request'
+import { useForm } from 'antd/es/form/Form'
+
 
 const DOMAIN = "http://106.52.167.166:8084"
-const API_RECOM = `${DOMAIN}/v1/rank/zone`   //TODO: = `${DOMAIN}/v1/recommendation/zone`
+const API_RECOMM = `${DOMAIN}/v1/rank/zone`   //TODO: = `${DOMAIN}/v1/recommendation/zone`
+const API_LATEST = `${DOMAIN}/v1/rank/zone`   //TODO: = `${DOMAIN}/v1/latest/zone`
 const API_IMG = `${DOMAIN}/v1/download/img`
-const getRankService = (params) => { return request(`${API_RECOM}`, { params }); }
-const getImgService = (params) => { return request(`${API_IMG}`, { params }); }
-
+const getRecommService = (params) => { return request(`${API_RECOMM}`, { params }); }
+const getLatestService = (params) => { return request(`${API_LATEST}`, { params }); }
 
 const LastestRecommand = () => {
-
     // Note that each item in the reocmmendatiaon list should have the following attributes: GID, game_name, like_num, DownloadNum, CommentNum, img, AuthorName
-    const [val_recommContent, set_recommContent] = useState([]);
+    const [val_listContent, set_listContent] = useState([]);
+    const [val_showOption,  set_showOption]  = useState(true); //"recommend" if true else "latest"
+    useEffect(
+        () => {
+            // const timer = null;
+            if(val_showOption){set_listContent( ()=>getRecommService({zone:"test", num:5}), 10000);}
+            else              {set_listContent( ()=>getLatestService({zone:"test", num:5}), 10000);}
+            // return () => {window.clearTimeout(timer);}
+            console.log(val_listContent)
+    },[val_showOption]);
+
+    // ===========================================================================
+    // ===========================================================================
 
     // Example: 
     // - From {"GID": 1, "game_name": "Apex", "like_num": 123, "DownloadNum": 456, "CommentNum": 10, "img": "https://tva1.sinaimg.cn/large/008i3skNly1guco1lvt96j61h90u0qbq02.jpg", "AuthorName": "Origin Corp."},
@@ -54,7 +67,8 @@ const LastestRecommand = () => {
     // ===========================================================================
 
     const LastestRecommandContentRow = ({ index, colNum }) => {
-        const fake_game_data = { "GID": 1, "game_name": "Apex", "like_num": 123, "DownloadNum": 456, "CommentNum": 10, "img": "https://tva1.sinaimg.cn/large/008i3skNly1guco1lvt96j61h90u0qbq02.jpg", "AuthorName": "Origin Corp. Electronic Arts" };
+        const fake_game_data = 
+            { "GID": 1, "game_name": "Apex", "like_num": 123, "DownloadNum": 456, "CommentNum": 10, "img": "https://tva1.sinaimg.cn/large/008i3skNly1guco1lvt96j61h90u0qbq02.jpg", "AuthorName": "Origin Corp. Electronic Arts" };
 
         return <ForLoop
             loopNum={colNum}
