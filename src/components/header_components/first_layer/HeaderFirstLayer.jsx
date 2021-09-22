24da98af165007d4f02f21b203eb9ec1343c59fa
SuoweiHu <suowei.h@gmail.com>
2021-09-22 15:19:58 +0800

    feat: attempt to use innerHTML approach to implment signup popup

--- src/components/header_components/first_layer/HeaderFirstLayer.jsx
+++ src/components/header_components/first_layer/HeaderFirstLayer.jsx
@@ -4,12 +4,13 @@
  * @modify date 2021-07-24 21:14:19
  */
 
-import { Popover, Button, Input, message} from 'antd';
+import { Popover, Button, Input, message } from 'antd';
 import React, { Component } from 'react';
 import { connect } from "react-redux";
 import { Row, Col } from 'reactstrap';
 import { Control, Form } from 'react-redux-form';
 import { Link } from 'react-router-dom';
+import { renderToString } from 'react-dom/server'
 
 import { fetchHeaderSearch } from '../../../redux/actions/creators/SearchAction'
 import { toggleLanguage } from '../../../redux/actions/creators/LocalizationAction'
@@ -46,20 +47,20 @@ const mapStateToProps = state => {
 
 // ==========================================================================================
 // Login service
-const DOMAIN = "http://106.52.167.166:8084" 
+const DOMAIN = "http://106.52.167.166:8084"
 const API_LOGIN = `${DOMAIN}/v1/user/login`
+const API_SINGUP = `${DOMAIN}/v1/user/register`
+
 const handle_loginRequest = (act, pwd) => {
     // console.log("Attempting to login via: " + val_email + " " + val_passw);
-    const loginPormise = getLoginService({email: act, password: pwd});
-    console.log("=================================================================");
-    console.log(loginPormise);
+    const loginPormise = getLoginService({ email: act, password: pwd });
     loginPormise.then(
-        function(value){
+        function (value) {
             // console.log('Login success');
             message.info('Login Successful', 2.0);
             // setTimeout(function(){history.push('/');message.info('Welcome '+val_email + ' !', 2.0);}, 2000);
         },
-        function(value){
+        function (value) {
             // console.log('Login failture');
             message.warn('Either your <email> or <password> is incorrect', 2.0);
         }
@@ -67,6 +68,28 @@ const handle_loginRequest = (act, pwd) => {
 }
 const getLoginService = (params) => {
     return request(`${API_LOGIN}`, {
+        method: "post",
+        data: params,
+        requestType: "form"
+    });
+}
+const handle_signupRequest = (mail, name, pwd) => {
+    // console.log("Attempting to signup via: " + email + " " + nickname + " " + password);
+    const signupPormise = getSignupSerive({ email: mail, nickname: name, password: pwd });
+    signupPormise.then(
+        function (value) {
+            console.log('Singup success');
+            message.info('Signup Successful', 2.0);
+            // setTimeout(function () { history.push('/'); message.info('Welcome ' + val_email + ' !', 2.0); }, 2000);
+        },
+        function (value) {
+            console.log('Signup failture');
+            message.warn('The email you used is already registered', 2.0);
+        }
+    )
+}
+const getSignupSerive = (params) => {
+    return request(`${API_SINGUP}`, {
         method: "post",
         data: params,
         requestType: "form"
@@ -97,6 +120,72 @@ class HeaderFirstLayer extends Component {
                         )
                     }
                     else {
+                        const SignupComponent_content = () => {
+                            return (
+                                <ul style={{ textAlign: 'left', padding: 0, margin: 0 }}>
+                                    {/* <li><a href="/login">Login</a></li> */}
+                                    {/* <li><a href="/signup">Sign-up</a></li> */}
+                                    <h3> Signup </h3>
+                                    Email:      <Input id="mainMenuSignup_mail" type="email"/> <br />
+                                    Nickname:   <Input id="mainMenuSignup_nick" /> <br />
+                                    Password:   <Input id="mainMenuSignup_pass" type="password"/>
+                                    {/* <li><a
+                                        href="/"
+                                        // style={{
+                                        //     textDecoration: 'underline',
+                                        //     color: 'blue'
+                                        // }}
+                                        // onClick={
+                                        //     () => {
+                                        //         const temp = document.getElementById("mainMenuPopup");
+                                        //         // temp.innerHTML = renderToString(<LoginComponent_content/>);
+                                        //         History.replaceState('/');
+                                        //     }
+                                        // }
+                                    >if you already have an account</a></li> <br /> */}
+                                    <li>-</li>
+                                    <Button onClick={
+                                        () => {
+                                            const account = document.getElementById("mainMenuSignup_mail");
+                                            const nickname = document.getElementById("mainMenuSignup_nick");
+                                            const password = document.getElementById("mainMenuSignup_pass");
+                                            handle_signupRequest(account.value, nickname.value, password.value)
+                                        }
+                                    }> Continue </Button>
+                                </ul>
+                            )
+                        }
+                        const LoginComponent_content = () => {
+                            return (
+                                <ul style={{ textAlign: 'left', padding: 0, margin: 0 }}>
+                                    {/* <li><a href="/login">Login</a></li> */}
+                                    {/* <li><a href="/signup">Sign-up</a></li> */}
+                                    <h3> Welcome </h3>
+                                    Account:  <Input id="mainMenuLogin_name" type="email"/> <br />
+                                    Password: <Input id="mainMenuLogin_pass" type="password"/>
+                                    <li><a
+                                        // href="/signup"
+                                        style={{
+                                            textDecoration: 'underline',
+                                            color: 'blue'
+                                        }}
+                                        onClick={
+                                            () => {
+                                                const temp = document.getElementById("mainMenuPopup");
+                                                temp.innerHTML = renderToString(<SignupComponent_content/>);
+                                            }
+                                        }
+                                    >if you don't have an account</a></li> <br />
+                                    <Button onClick={
+                                        () => {
+                                            const account = document.getElementById("mainMenuLogin_name");
+                                            const passwrd = document.getElementById("mainMenuLogin_pass");
+                                            handle_loginRequest(account.value, passwrd.value)
+                                        }
+                                    }> Login </Button>
+                                </ul>
+                            )
+                        }
                         return (
                             // <Link to='/TobeChanged2' key={btnInfo[0]} className={'my-link me-lg-4 me-xl-5'}>
                             <Popover
@@ -104,23 +193,9 @@ class HeaderFirstLayer extends Component {
                                     ""
                                 }
                                 content={
-                                    <ul id="mainMenuPopup" style={{textAlign:'left', padding: 0, margin: 0}}>
-                                        {/* <li><a href="/login">Login</a></li> */}
-                                        {/* <li><a href="/signup">Sign-up</a></li> */}
-                                        <h3> Welcome </h3>
-                                        Account:  <Input id="mainMenuLogin_name"/> <br/>
-                                        Password: <Input id="mainMenuLogin_pass"/>
-                                        <li><a 
-                                            href="/signup"
-                                        >if you don't have an account</a></li> <br />
-                                        <Button onClick={
-                                            () => {
-                                                const account = document.getElementById("mainMenuLogin_name");
-                                                const passwrd = document.getElementById("mainMenuLogin_pass");
-                                                handle_loginRequest(account.value, passwrd.value)
-                                            }
-                                        }> Login </Button>
-                                    </ul>
+                                    <div id="mainMenuPopup" >
+                                        <LoginComponent_content />
+                                    </div>
                                 }
                                 key={btnInfo[0]}
                                 className={'my-link me-lg-4 me-xl-5'}
