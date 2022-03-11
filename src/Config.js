560aa48599ebbf2ca7146d0fc1b9827472bf8ee7
Huiying Hu <u7150345@anu.edu.au>
2022-03-11 13:32:50 +0800

    feat: add collect interaction, print zip directory

--- src/Config.js
+++ src/Config.js
@@ -4,14 +4,21 @@
  * @modify date 2021-09-24 11:32:25
  */
 
-// export const serverUrl = 'http://localhost:3001/'
-const API_URL = "http://175.178.159.131:8084"
+
+const DOMAIN = "http://175.178.159.131:8084"
 const API_WT = "http://rinato.ticp.vip"
-export const API_RANK = `${API_URL}/v1/rank/zone`
-export const API_IMG = `${API_URL}/v1/download/img`
-export const API_RANK_DOWNLOAD = `${API_URL}/v1/rank/download`
+
+//Homepage & GameDisplay URLs
+export const API_RANK = `${DOMAIN}/v1/rank/zone`
+export const API_IMG = `${DOMAIN}/v1/download/img`
+export const API_RANK_DOWNLOAD = `${DOMAIN}/v1/rank/download`
 export const API_PRODUCT = `${API_WT}/v1/get/product`
-export const API_LIKE_CLICK = `${API_URL}/v1/like/click`
-export const API_LIKE_CHECK = `${API_URL}/v1/like/check`
-export const API_LIKE_NUM = `${API_URL}/v1/like/num`
-export const DOWNLOAD_GAME = `${API_URL}/v1/download/game`
+export const API_LIKE_CLICK = `${DOMAIN}/v1/like/click`
+export const API_LIKE_NUM = `${DOMAIN}/v1/like/num`
+export const API_LIKE_CHECK = `${DOMAIN}/v1/like/check`
+export const API_COLLECT_CLICK = `${DOMAIN}/v1/collect/click`
+export const API_COLLECT_CHECK = `${DOMAIN}/v1/collect/check`
+export const API_COLLECT_NUM = `${DOMAIN}/v1/collect/num`
+export const DOWNLOAD_GAME = `${DOMAIN}/v1/download/game`
+
+//Login URLs, etc.
