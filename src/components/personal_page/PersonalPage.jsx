c083d2e3bca611990393a29e61b77c3a18e8ac9b
Huiying Hu <u7150345@anu.edu.au>
2022-04-08 16:33:57 +0800

    feat: layout for PersonalPage

--- src/components/personal_page/PersonalPage.jsx
+++ src/components/personal_page/PersonalPage.jsx
@@ -1,6 +1,12 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Avatar, } from 'antd'
 import "./PersonalPage.scss"
+import Header from '../header_components/Header.jsx'
+import RankWrapper from '../rank/component/RankWrapper/RankWrapper';
+import RankBlock from '../rank/component/RankBlock/RankBlock';
+import { getRankList } from '../../service/Rank'
+import { API_IMG } from '../../Config.js';
+
 
 const gameDetail = {
     creator: "Creator",
@@ -13,18 +19,80 @@ const gameDetail = {
     comments: "2342 comments"
 }
 
+const menuLabels = ["HOME", "GAMES", "POSTS", "FOLLOWING", "SETTINGS"]
+
+const CoverBlock = ({ game_name, like_num, playCount, AuthorName, img }) => {
+    return <div className='cover-block'>
+        <img src={`${API_IMG}?img_name=${img}`} />
+        <div>{game_name}</div>
+        <div>{`${playCount} played · ${like_num} liked`}</div>
+        <div>{AuthorName}</div>
+    </div>
+}
+
 const PersonalPage = (props) => {
+    const [rankList, setRankList] = useState([])
+    useEffect(() => {
+        const getRank = async () => {
+            const result = await getRankList({
+                zone: "test",
+                num: 5,
+            });
+            setRankList(result.List);
+        }
+        getRank();
+    }, [])
+
     return (
         <>
-            <div class="personal-page-container">
-                <div>
-                    <a href="">
-                        <Avatar src="images/header/header_avatar_btn.svg"></Avatar>
-                    </a>
-
+            <Header></Header>
+            <div className="personal-page-container">
+                <div className="header">
+                    <div className="creator">
+                        <div>
+                            <a href="">
+                                <Avatar size={48} src="images/header/header_avatar_btn.svg"></Avatar>
+                            </a>
+                        </div>
+                        <div className="creator-right">
+                            <span style={{ fontSize: '18px', display: "block" }}>{gameDetail.creator}</span>
+                            <span>{gameDetail.subscribers}</span>
+                        </div>
+                    </div>
+                    <div className="menu">
+                        {menuLabels.map(label => (<div className='menu-tab'>{label}</div>))}
+                    </div>
                 </div>
-                <div className='creator'>
-                    <span style={{ fontSize: '14px', display: "block" }}>{gameDetail.creator}</span>
+                <div className='content'>
+                    <div className='pinned-game'>
+                        {rankList.slice(0, 1).map((data, index) =>
+                            <div className='pinned-game-wrapper'>
+                                <CoverBlock playCount={data.playCount}
+                                    AuthorName={data.AuthorName}
+                                    img={data.img}
+                                    like_num={data.like_num}
+                                    game_name={data.game_name} />
+                            </div>
+                        )}
+                    </div>
+
+                    <div className='works'>
+                        <div>
+                            <span className='works-header'>Games</span>
+                            <a>View All</a>
+                        </div>
+                        <div className='cover-block-row'>
+                            {rankList.map((data, index) =>
+                                <div className='cover-block-item'>
+                                    <CoverBlock playCount={data.playCount}
+                                        AuthorName={data.AuthorName}
+                                        img={data.img}
+                                        like_num={data.like_num}
+                                        game_name={data.game_name} />
+                                </div>
+                            )}
+                        </div>
+                    </div>
                 </div>
             </div>
         </>
